860555a17524bf478d77988bd57055fbc838c3f3
minhhieu392 <minhhieu030920@gmail.com>
2023-03-17 18:01:37 +0700

    delete gm

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import 'dotenv/config'
 import Websocket from './config/websocket';
 import RedisClient from './config/redis';
-import GameManager from './games/gameManager';
 import { BONUS_GAME_DATA_REDIS_TYPE } from './utils/constants';
 import { clearUserFreeRouletteTurn } from './server/services/outgame/clearUserFreeRouletteTurn.service';
 import RedisMatching from './config/redisMatching';
 
 const init = async () => {
-  await RedisClient.initializer();
-  await RedisMatching.initializer();
-  clearUserFreeRouletteTurn()
-  const websocket = new Websocket();
-  websocket.handlers();
-  global.gameManager = new GameManager();
-  await RedisClient.publish("bonus-game-server-start", { type: BONUS_GAME_DATA_REDIS_TYPE.ALL }); // lấy thông tin danh sách bonus game ngay khi tạo server
+    await RedisClient.initializer();
+    await RedisMatching.initializer();
+    clearUserFreeRouletteTurn()
+    const websocket = new Websocket();
+    websocket.handlers();
+    await RedisClient.publish("bonus-game-server-start", { type: BONUS_GAME_DATA_REDIS_TYPE.ALL }); // lấy thông tin danh sách bonus game ngay khi tạo server
 }
 
 init();
